fecdfa88d0c027b3ed2c8e4d558f3c9e08ecd82c
Nick <ndavis1391@gmail.com>
2023-05-23 20:22:26 -0400

    final readme

--- src/pages/EditOutfitPage/EditOutfit.js
+++ src/pages/EditOutfitPage/EditOutfit.js
@@ -4,7 +4,25 @@ export default function EditOutfitPage() {
 
     return(
         <div>
-            
+            <form onSubmit={handleSubmit}>
+            Event: <input type="text" name="event" onChange={handleChange} value={formData.event}/>
+            <br />
+            Top: <input type="text" name="top" onChange={handleChange} value={formData.top}/>
+            <br />
+            Top color: <input type="text" name="topColor" onChange={handleChange} value={formData.topColor} />
+            <br />
+            Bottom: <input type="text" name="bottom" onChange={handleChange} value={formData.bottom}/> 
+            <br />
+            Bottom Color: <input type="text" name="bottomColor" onChange={handleChange} value={formData.bottomColor}/> 
+            <br />
+            Footwear: <input type="text" name="feet" onChange={handleChange} value={formData.feet}/>
+            <br />
+            Footwear Color: <input type="text" name="feetColor" onChange={handleChange} value={formData.feetColor}/>
+            <br />
+            Description: <input type="text" name="description" onChange={handleChange} value={formData.description}/>
+            <br />
+            <input type="submit" value="Submit" />       
+          </form>
         </div>
     )
 }
